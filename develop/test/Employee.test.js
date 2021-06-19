4bcd651c6f0e89e5299675d57f84aba0a50cae7a
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-06-19 15:59:33 -0700

    employees test fail

--- develop/test/Employee.test.js
+++ develop/test/Employee.test.js
@@ -19,13 +19,30 @@ test("Can set id via constructor argument", () => {
 
 test("Can set email via constructor argument", () => {
   const testValue = "test@test.com";
-  const e = new Employee("Foo", 1, testValue);
+  
   expect(e.email).toBe(testValue);
 });
 
 test("Can get name via getName()", () => {
   const testValue = "Alice";
-  const e = new Employee(testValue);
+  
   expect(e.getName()).toBe(testValue);
 });
 
+test("Can get id via getId()", () => {
+  const testValue = 100;
+ 
+  expect(e.getId()).toBe(testValue);
+});
+
+test("Can get email via getEmail()", () => {
+  const testValue = "test@test.com";
+ 
+  expect(e.getEmail()).toBe(testValue);
+});
+
+test("getRole() should return \"Employee\"", () => {
+  const testValue = "Employee";
+  
+  expect(e.getRole()).toBe(testValue);
+});
