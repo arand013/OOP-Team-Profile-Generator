83733557bb1ad1eb1e7b7dbe35d10e04457be2b8
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-06-19 19:18:31 -0700

    manager questions

--- develop/app.js
+++ develop/app.js
@@ -10,3 +10,40 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+
+
+// -----
+
+//empty array that can store the team members in, Name ID and Email are in every array becase each array becasue that information is prevalent to every employee 
+const teamArray = [];
+
+// questions for different teams members:
+
+// Manager: 
+const managerQuestions = [
+
+    {
+        type: 'input',
+        name: 'managerName',
+        message: 'Please enter the name of the manager of this team, or your name if you are the manager of this team. '
+    },
+
+    {
+        type: 'input',
+        name: 'managerID',
+        message: 'What is this managers ID number, enter your ID number if you are the manager of this team'
+    },
+
+    {
+        type: 'input',
+        name: 'managerEmail',
+        message: 'What is this managers Email adress, enter your email adress if you are the manager of this team'
+    },
+
+    {
+        type: 'input',
+        name: 'office',
+        message: 'What is this managers office number? if you are the manager of this team, enter your office number'
+    },
+]
+
